298f05724519878c6cc658937dbf4d39337c2b61
Gercil Macbook <gerciljunio@gmail.com>
2023-05-13 11:01:52 -0300

    add bar
    Signed-off-by: Gercil Macbook <gerciljunio@gmail.com>

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,5 +1,10 @@
 <template>
   <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>TItle</ion-title>
+            </ion-toolbar>
+        </ion-header>
         <ion-content>
             <ion-searchbar></ion-searchbar>
             <ion-card>
