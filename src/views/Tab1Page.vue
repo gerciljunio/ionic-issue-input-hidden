7cd37282307111b25ee3068a4a4c051d01525def
Gercil Macbook <gerciljunio@gmail.com>
2023-05-13 10:44:30 -0300

    ionic issue
    Signed-off-by: Gercil Macbook <gerciljunio@gmail.com>

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,36 @@
+<template>
+  <ion-page>
+        <ion-content>
+            <ion-searchbar></ion-searchbar>
+            <ion-card>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <ion-card-header>
+                    <ion-card-title>Card Title</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                </ion-card-header>
+    
+                <ion-card-content>
+                    Here's a small text description for the card content. Nothing more, nothing less.
+                </ion-card-content>
+            </ion-card>
+            <ion-searchbar></ion-searchbar>
+            <ion-card>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <ion-card-header>
+                    <ion-card-title>Card Title</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                </ion-card-header>
+    
+                <ion-card-content>
+                    Here's a small text description for the card content. Nothing more, nothing less.
+                </ion-card-content>
+            </ion-card>
+            <ion-searchbar></ion-searchbar>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
